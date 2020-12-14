2d0e29fe23d08fe4ba6934e524439094e5f03256
Muhammad <imuhammadibragimov1994@gmail.com>
2020-12-14 16:38:05 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Redirect,
+} from 'react-router-dom';
 
-function App() {
-  return (
-    <div>
-      Root
-    </div>
-  );
+import Main from 'layout/Main';
+import Auth from 'layout/Auth';
+
+export default function App() {
+    return (
+        <Router>
+            <div>
+                <Switch>
+                    <Route exact path='/auth'>
+                        <Auth />
+                    </Route>
+                    <Route path='/main'>
+                        <Main />
+                    </Route>
+
+                    <Redirect to='/main' />
+                </Switch>
+            </div>
+        </Router>
+    );
 }
-
-export default App;
