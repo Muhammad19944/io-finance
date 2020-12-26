0ebdbb078997879a1e2b2548d9e8f78d22a660f8
Odinayev <imuhammadibragimov1994@gmail.com>
2020-12-27 00:15:32 +0500

    firebase installed

--- src/index.js
+++ src/index.js
@@ -2,13 +2,41 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import makeServer from 'server/index'
+import makeServer from "server/index";
 
 import "assets/css/index.css";
 
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+// import firebase from "firebase/app";
+// If you are using v7 or any earlier version of the JS SDK, you should import firebase using namespace import
+// import * as firebase from "firebase/app"
+
+// If you enabled Analytics in your project, add the Firebase SDK for Analytics
+// import "firebase/analytics";
+
+// Add the Firebase products that you want to use
+// import "firebase/auth";
+// import "firebase/firestore";
+
 if (process.env.NODE_ENV === "development") {
-	makeServer({ environment: "development" })
+	makeServer({ environment: "development" });
 }
+
+// TODO: Replace the following with your app's Firebase project configuration
+// For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
+
+// var firebaseConfig = {
+// 	apiKey: "AIzaSyDSNrzprjAssIo-ZfLskMFrZ3TmtavOsNU",
+// 	authDomain: "io-finance.firebaseapp.com",
+// 	projectId: "io-finance",
+// 	storageBucket: "io-finance.appspot.com",
+// 	messagingSenderId: "995386125950",
+// 	appId: "1:995386125950:web:c87261a618f2627b2eb22f",
+// 	measurementId: "G-01QLKKV5K1",
+// };
+
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
 	<React.StrictMode>
